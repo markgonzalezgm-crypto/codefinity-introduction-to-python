4fd37bfd6f5314c4b16e6cdc91b79c179b01ad0c
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-17 12:29:12 -0700

    Add comments on commenting/uncommenting code
    Added comments to explain how to comment/uncomment lines.

---
+++ getting_started/printing_basics/comments.py
@@ -0,0 +1,4 @@
+# You can quickly comment or uncomment lines of code by selecting them and pressing Ctrl + / on Windows or Command + / on Mac.
+print("Blueberries")   
+# print("Oranges")     
+print("Ranch dressing") 

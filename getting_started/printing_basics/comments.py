e71fa9e2f8a32bb09d166d24eff4873bc12c06c7
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-17 12:37:15 -0700

    Delete getting_started/printing_basics/comments.py

--- getting_started/printing_basics/comments.py
+++
@@ -1,4 +0,0 @@
-# You can quickly comment or uncomment lines of code by selecting them and pressing Ctrl + / on Windows or Command + / on Mac.
-print("Blueberries")   
-# print("Oranges")     
-print("Ranch dressing") 

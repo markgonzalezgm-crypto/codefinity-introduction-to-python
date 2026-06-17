50ef8ed6f228b49f8d882eb6ac34f5dc7a8d2132
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-17 12:50:14 -0700

    Create ex_1.py

---
+++ variables_and_types/variable_assignment/ex_1.py
@@ -0,0 +1,10 @@
+# Storing different types of data in variables
+item_name = "Banana"
+item_price = 0.50
+item_stock_quantity = 100
+in_stock = True
+
+print("Item:", item_name)
+print("Price: $", item_price)
+print("Quantity:", item_stock_quantity)
+print("In stock:", in_stock)

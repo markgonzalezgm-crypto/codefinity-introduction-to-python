8222e51fa68da05706bbc883e9e72b36165325cf
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-18 12:31:44 -0700

    Create Ex_2.py

---
+++ variables_and_types/variable_naming/Ex_2.py
@@ -0,0 +1,6 @@
+# Incorrect variable names 
+1item = "Banana" # Variables cannot start with a number. Instead try `item_one`, or `oneItem`
+item-name = "Orange" # Variables cannot contain a dash (-), use underscores (_) instead. For example `item_name` is a valid variable 
+print = 5.0 # You cannot use reserved keywords as a variable
+# But you can use these words in combination with others to name a variable
+# For example, `print_quantity = 5.0` is valid.

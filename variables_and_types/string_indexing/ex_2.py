9fdd05534812cdcbd72b7c862cdabc89a9e52fc6
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-18 12:40:29 -0700

    Create ex_2.py

---
+++ variables_and_types/string_indexing/ex_2.py
@@ -0,0 +1,10 @@
+store_name = "Green Valley Market"
+
+# Find the length of the string, which includes spaces
+length_of_name = len(store_name)  # Includes spaces in the count
+
+# Accessing a character in a position after a space
+character_after_space = store_name[6]  # 'V'
+
+print("Length of store name:", length_of_name)
+print("Character after the space:", character_after_space)

8294c0d63b01e90265408ee20cbcd226d2f82089
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-18 12:41:03 -0700

    Create ex_1.py

---
+++ variables_and_types/string_manipulation/ex_1.py
@@ -0,0 +1,7 @@
+fruit = "Strawberries"
+
+# Slicing the string to get "Straw"
+# Remember, the 'w' is indexed at 4 but if we want to include it in the slice, we need to go up to 5 
+sliced_fruit = fruit[0:5]
+
+print("Sliced part:", sliced_fruit)

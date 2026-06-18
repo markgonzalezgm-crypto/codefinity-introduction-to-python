6944c66793680f6cc40c9089308da7d4960329cb
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-18 12:39:14 -0700

    Create ex_1.py

---
+++ variables_and_types/variable_calculations/ex_1.py
@@ -0,0 +1,6 @@
+# Calculate the total cost of 100 cupcakes
+item_price = 0.50
+item_quantity = 100
+total_cost = item_price * item_quantity
+
+print("Total cost for", item_quantity, "cupcakes is $", total_cost)

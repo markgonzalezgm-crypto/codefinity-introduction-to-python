cd8d3fcc8ef0aa8960e5f67b59b81a09e6c7e359
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-18 12:44:44 -0700

    Create ex_1.py

---
+++ variables_and_types/data_types/ex_1.py
@@ -0,0 +1,11 @@
+# Displaying integers
+print(type(25))
+
+# Displaying floating-point numbers
+print(type(6.25))
+
+# Displaying strings
+print(type("Olive Oil"))
+
+# Displaying booleans 
+print(type(120 > 95))

1d047118b9791c271554b28f9669d0d642427f55
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-18 12:31:20 -0700

    Add example of correct variable naming in Python

---
+++ variables_and_types/variable_naming/ex_1.py
@@ -0,0 +1,6 @@
+# Correct variable names
+item_name = "Apple"
+_item_price = 0.99
+item1_quantity = 10
+storeName = "Green Valley Groceries"
+print(item_name, _item_price, item1_quantity, storeName)

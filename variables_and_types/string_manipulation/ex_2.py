9a03a3875e6d83f532d9337a8e46d85ae518594e
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-18 12:41:46 -0700

    Create ex_2.py

---
+++ variables_and_types/string_manipulation/ex_2.py
@@ -0,0 +1,8 @@
+# Concatenating strings
+part1 = "Straw"
+part2 = "berry"
+new_word = part1 + part2  # "Strawberry"
+print("Concatenated word:", new_word)
+
+# If you want to separate the words with a space, you need to add " " between the two parts
+print(part1 + " " + part2)  # "Straw berry"

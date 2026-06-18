a1dd9db4dd69ccb48930a1a36450815fc9370250
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-18 12:45:50 -0700

    Add example for variable types in Python

---
+++ variables_and_types/data_types/ex_2.py
@@ -0,0 +1,9 @@
+item_name = "Milk"
+item_price = 3.14
+item_quantity = 0
+in_stock = False
+
+print(type(item_quantity))  # <class 'int'>
+print(type(item_name))  # <class 'str'>
+print(type(item_price))  # <class 'float'>
+print(type(in_stock))  # <class 'bool'>

9d300f40d02126459d131136eb2925903646c6cc
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-18 12:51:00 -0700

    Create ex_5.py

---
+++ variables_and_types/string_manipulation/ex_5.py
@@ -0,0 +1,5 @@
+greeting = "Hello customer, welcome to the grocery store!"
+name = "Alice"
+personal_greeting = greeting[0:5] + " " + name + "," + greeting[15:]
+
+print(personal_greeting)  # Hello Alice, welcome to the grocery store!

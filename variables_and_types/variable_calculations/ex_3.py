8e042d6c7bc7a948b63276d93207b864bcca9552
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-18 12:49:41 -0700

    Create ex_3.py

---
+++ variables_and_types/variable_calculations/ex_3.py
@@ -0,0 +1,6 @@
+# Using variables to calculate discounts
+item_price = 19.99
+discount_percentage = 0.2
+discounted_price = item_price * (1 - discount_percentage)
+
+print("Discounted price: $", discounted_price)

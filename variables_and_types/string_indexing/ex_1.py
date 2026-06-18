f1f79d4da69832298d98f4ad69e3b7e75ea7de75
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-06-18 12:40:06 -0700

    Add example of string indexing in ex_1.py
    Demonstrate string indexing by accessing first and last characters.

---
+++ variables_and_types/string_indexing/ex_1.py
@@ -0,0 +1,8 @@
+grocery_item = "Milk"
+
+# Accessing the first and last character using indexing
+first_character = grocery_item[0]  # 'M'
+last_character = grocery_item[-1]  # 'k', using negative indexing for the last character
+
+print("First character:", first_character)
+print("Last character:", last_character)

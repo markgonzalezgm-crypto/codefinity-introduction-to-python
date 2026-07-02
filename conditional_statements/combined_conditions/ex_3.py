2975bbf2fef3cf6f2fcf0c3970f747413ac1b4f1
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-07-01 20:17:59 -0700

    Add check for item perishable status

---
+++ conditional_statements/combined_conditions/ex_3.py
@@ -0,0 +1,7 @@
+is_perishable = True
+
+# Check if the item is NOT perishable
+long_shelf_life = not is_perishable
+
+# Print the result
+print("Does the item need to be sold quickly?", long_shelf_life) 

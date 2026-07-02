71cc6ba9bbb4a6d3a8d8b91d00c1e4914f925f83
markgonzalezgm-crypto <mark.gonzalez.gm@gmail.com>
2026-07-01 20:17:39 -0700

    Add check for seasonal or holiday items

---
+++ conditional_statements/combined_conditions/ex_2.py
@@ -0,0 +1,8 @@
+seasonal_item = False
+holiday_item = True
+
+# Check if the item is seasonal or discounted
+temporary_stock = seasonal_item or holiday_item
+
+# Print the result
+print("Is this a seasonal or holiday item?", temporary_stock) 
